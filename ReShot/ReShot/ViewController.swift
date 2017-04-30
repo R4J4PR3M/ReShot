4765d9c4a4fb70c8a678d672ec6e6645041041a1
Raj <rpsadarangani@me.com>
2017-04-30 16:06:27 -0400

    Setting up project

---
+++ ReShot/ReShot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReShot
+//
+//  Created by Raja Prem Sadarangani on 4/30/17.
+//  Copyright © 2017 Per4Mance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
